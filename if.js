f4accc767e8e83547afc863ddaaa2aa220138afe
fawziaruma <fawziaruma0652@gmail.com>
2022-07-25 10:23:45 +0600

    first commit

---
+++ if.js
@@ -0,0 +1,28 @@
+var dayTime = 12;
+var nightTime = 13;
+if (dayTime > nightTime){
+    console.log('it would be a good day')
+}
+else{
+    console.log('this is good day')
+}
+
+var dressPrice = 2000;
+var myMoney = 2200;
+if(myMoney > dressPrice){
+    console.log('i will buy the dress')
+}
+
+if(3 < 5){
+    console.log('it true')
+}
+
+var iphonePrice = 10000;
+var myMoney = 1000;
+if(iphonePrice < myMoney){
+    console.log('iphone kinbo')
+}
+else{
+    console.log('kinbo na')
+}
+
